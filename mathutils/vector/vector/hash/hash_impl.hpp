2aeab5a1d2e36ede4ff10dd2276dacfbe0f26561
rxnew <rxnew.axdseuan@gmail.com>
2017-01-09 01:28:42 +0900

    バグ修正

--- mathutils/vector/vector/hash/hash_impl.hpp
+++ mathutils/vector/vector/hash/hash_impl.hpp
@@ -6,7 +6,7 @@ auto hash<mathutils::Vector<dim, Real>>::
 operator()(mathutils::Vector<dim, Real> const& obj) const noexcept -> size_t {
   auto hash_value = size_t(0);
   for(auto i = 0; i < dim; ++i) {
-    hash_value ^= hash<Real>((*this)[i]);
+    hash_value ^= hash<Real>(obj[i]);
   }
   return hash_value;
 }
