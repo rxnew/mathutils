b8d6edacd33322bd6c0aeb2d5aef4c0ba0455cbc
rxnew <rxnew.axdseuan@gmail.com>
2017-01-09 01:15:54 +0900

    バグ修正

--- mathutils/vector/polyhedron/polyhedron_impl.hpp
+++ mathutils/vector/polyhedron/polyhedron_impl.hpp
@@ -109,6 +109,6 @@ auto hash<mathutils::Polyhedron<dim, Real, VectorT>>::
 operator()(mathutils::Polyhedron<dim, Real, VectorT> const& obj) const noexcept
   -> size_t {
   using Vector = typename mathutils::Polyhedron<dim, Real, VectorT>::Vector;
-  return hash<Vector>(obj.get_size()) ^ hash<Vector>(obj.get_position());
+  return hash<Vector>()(obj.get_size()) ^ hash<Vector>()(obj.get_position());
 }
 }
